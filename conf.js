4cd52bebc817eb4376b1711b8b2ad7c24ba1d6eb
mazehao001 <57321827+mazehao001@users.noreply.github.com>
2019-11-13 23:32:09 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '110',
+  ver: '112',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -34,8 +34,7 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
-      hidden: true,
+      label: '免费节点',
       lines: {
         // 收费版（高权重）
         'node-cfworker-2.etherdream.com': 4,
@@ -43,6 +42,7 @@ jsproxy_config({
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         'b.007.workers.dev': 1,
+        'google.cqupt.workers.dev': 1,
         'b.hehe.workers.dev': 1,
         'b.lulu.workers.dev': 1,
         'b.jsproxy.workers.dev': 1,
@@ -87,9 +87,6 @@ jsproxy_config({
   url_handler: {
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
     },
     'http://haha.com/': {
       content: 'Hello World'
